a8b4c43f4c5a067ae310155bbeeb2372ca8e1ca5
isina_nej <isina4501@gmail.com>
2025-08-16 10:02:19 +0330

    Refactor DNS ping functionality: remove old implementations and update dependencies
    - Deleted obsolete files: dns_ping_helper_new.dart, dns_ping_sequential.dart, dns_ping_simultaneous.dart.
    - Updated pubspec.yaml to remove dart_ping dependency and added webview_flutter dependency.
    - Updated pubspec.lock to reflect changes in dependencies.

--- lib/path/path.dart
+++ lib/path/path.dart
@@ -8,7 +8,8 @@ export '../api/models/update_info.dart';
 export '../api/services/update_api_service.dart';
 export '../api/services/api_client.dart';
 export '../api/services/dns_api_service.dart';
-
+export '../api/services/dns_usage_api_service.dart';
+export '../api/models/dns_usage_request.dart';
 // constants
 export '../constants/dns_constants.dart';
 
@@ -22,9 +23,10 @@ export '../models/google_connectivity_result.dart';
 
 // screens
 export '../screens/homepage.dart';
+// export '../screens/speed_checker_plugin.dart';
+export '../screens/dns_list.dart';
 export '../screens/speed_test_page.dart';
-export '../screens/dns_list_new.dart';
-
+export '../screens/settings_page.dart';
 
 // services
 export '../services/dns_service.dart';
@@ -37,7 +39,8 @@ export '../styles/app_text_styles.dart';
 export '../styles/app_themes.dart';
 export '../styles/language_manager.dart';
 export '../styles/theme_manager.dart';
-
+export '../widgets/notification_bell.dart';
+export '../widgets/custom_drawer.dart';
 // utils
 export '../utils/dns_validator.dart';
 export '../utils/format_utils.dart';
